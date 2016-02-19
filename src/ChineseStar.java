326ca6732f55f72b085293ab52735ffdd03fcc32
aubem <aubem@PC-B103-07.iut-valence.fr>
2016-02-19 14:28:46 +0100

    launcher creation

---
+++ src/ChineseStar.java
@@ -0,0 +1,18 @@
+
+public class ChineseStar {
+	
+	public ChineseStar(){
+		
+	}
+	
+/** start method
+ * start a new game
+ * */
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	
+/* application launcher*/
+	public static void main(String[] args) {
+		new ChineseStar().start();
+		
+	}
+
+}
