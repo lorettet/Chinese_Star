caa6e81348df9e5656c35edd3085def8b12b5f93
sebastienjean <baz.jean@gmail.com>
2016-03-07 11:41:15 +0100

    Renamed project, remove unused code added some TODO

--- src/ChineseStar.java
+++ src/ChineseStar.java
@@ -1,15 +1,22 @@
-
+// TODO format your code correctly
+// TODO write comment
+// TODO rename this class (ask for advice)
 public class ChineseStar {
 	
+
+	// TODO write comment	
 	public ChineseStar(){
 		
 	}
 
+// TODO consider removing this method (ask why)
 /** Asks user how many players will play */
 	public int askNbrPlayer(){
 		// TODO Auto-generated method stub
 	}
 	
+// TODO clean comment (ask for advice)
+// TODO this method should have no parameter
 /** play method
  * play a new game
  * set the tray
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+// TODO update README.md (what/who)
 
+// TODO write comment
 public class Main {
-	
+
+// TODO write a "javadoc" comment
 /* application launcher*/
 	public static void main(String[] args) {
 		
